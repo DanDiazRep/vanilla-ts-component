3fbcef8923ae3aaf0a40594bd5d4558388913f55
Daniel <zrelick@gmail.com>
2023-05-23 19:11:16 +0200

    adding mock data and fetching the data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 import './styles/main.css';
+
+interface CardData {
+    id: number;
+    name: string;
+    price: number;
+    imageURL: string;
+}
+
+const fetchData = async () => {
+    let data: CardData[];
+    try {
+        const response = await import('./data.json');
+        data = response.default;
+        return data;
+    }
+    catch (error) {
+        console.log("Error loading the data:", error);
+        return [];
+    }
+
+}
+
+fetchData().then(data => { console.log(data) });
